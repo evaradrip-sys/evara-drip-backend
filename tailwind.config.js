502be3f1d8556ce5e0127be90dad0f1aeb4daa82
omprakashsingh <munnato822@mail.com>
2025-09-20 21:34:22 +0530

    Tailwind Added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/main/webapp/**/*.{html,js,ts,jsx,tsx}', // scan all React + HTML files
+  ],
+  theme: {
+    extend: {}, // customize if needed later
+  },
+  plugins: [],
+};
